26287807f219a0bbeafa3f54ed0d42f45a6f377b
Ryan Dunn <contact@ryandunn.dev>
2020-10-31 16:11:33 +0000

    add describe to tests

--- src/tests/ChoosePreferences.test.js
+++ src/tests/ChoosePreferences.test.js
@@ -8,29 +8,31 @@ const desiredOrder = ["v3", "v4", "v2", "v1"];
 const findIndexInDesiredOrder = (node) =>
   desiredOrder.findIndex((str) => node.innerHTML.indexOf(str) !== -1);
 
-test("chosen values can be sorted", async () => {
-  const setPage = jest.fn();
-  const { getByTestId } = render(ChoosePreferences, {
-    values: originalOrder,
-    setPage,
+describe("Choose preferences screen", () => {
+  test("Chosen values can be sorted", async () => {
+    const setPage = jest.fn();
+    const { getByTestId } = render(ChoosePreferences, {
+      values: originalOrder,
+      setPage,
+    });
+
+    // choose preferences based on desired order
+    let i = 0;
+    while (setPage.mock.calls.length === 0 && i < 14) {
+      // get index of each choice
+      const choice1 = getByTestId("choice1");
+      const choice2 = getByTestId("choice2");
+      const i1 = findIndexInDesiredOrder(choice1);
+      const i2 = findIndexInDesiredOrder(choice2);
+
+      // press the preferred button
+      const choice = i1 < i2 ? choice1 : choice2;
+      await fireEvent.click(choice);
+      i++;
+    }
+
+    // check that the step was completed accurately
+    expect(setPage.mock.calls.length).toBe(1);
+    expect(setPage.mock.calls[0][1]).toEqual(desiredOrder);
   });
-
-  // choose preferences based on desired order
-  let i = 0;
-  while (setPage.mock.calls.length === 0 && i < 14) {
-    // get index of each choice
-    const choice1 = getByTestId("choice1");
-    const choice2 = getByTestId("choice2");
-    const i1 = findIndexInDesiredOrder(choice1);
-    const i2 = findIndexInDesiredOrder(choice2);
-
-    // press the preferred button
-    const choice = i1 < i2 ? choice1 : choice2;
-    await fireEvent.click(choice);
-    i++;
-  }
-
-  // check that the step was completed accurately
-  expect(setPage.mock.calls.length).toBe(1);
-  expect(setPage.mock.calls[0][1]).toEqual(desiredOrder);
 });
--- src/tests/SelectValues.test.js
+++ src/tests/SelectValues.test.js
@@ -8,39 +8,41 @@ const MAX_ITEMS = 12;
 
 const values = Array.from({ length: 15 }, (_, i) => `t${i}`);
 
-test("items in list are selectable", async () => {
-  const setPage = jest.fn();
-  const { getByText, getByTestId } = render(SelectValues, {
-    values,
-    setPage,
+describe("Select values screen", () => {
+  test("Items in list are selectable", async () => {
+    const setPage = jest.fn();
+    const { getByText, getByTestId } = render(SelectValues, {
+      values,
+      setPage,
+    });
+
+    // first item exists
+    expect(getByText(values[0])).toBeInTheDocument();
+
+    // get proceed button
+    const proceed = getByTestId("proceed");
+    expect(proceed).toBeDisabled();
+
+    // select enough items
+    for (let i = 0; i < MIN_ITEMS; i++) {
+      await fireEvent.click(getByTestId(values[i]));
+    }
+    expect(proceed).toBeEnabled();
+    await fireEvent.click(proceed);
+    expect(setPage.mock.calls.length).toBe(1);
+    expect(setPage.mock.calls[0][1]).toEqual(values.slice(0, MIN_ITEMS));
+
+    // select too many items
+    for (let i = MIN_ITEMS; i <= MAX_ITEMS; i++) {
+      await fireEvent.click(getByTestId(values[i]));
+    }
+    expect(proceed).toBeDisabled();
+
+    // unselect an item
+    await fireEvent.click(getByTestId(values[0]));
+    expect(proceed).toBeEnabled();
+    await fireEvent.click(proceed);
+    expect(setPage.mock.calls.length).toBe(2);
+    expect(setPage.mock.calls[1][1]).toEqual(values.slice(1, MAX_ITEMS + 1));
   });
-
-  // first item exists
-  expect(getByText(values[0])).toBeInTheDocument();
-
-  // get proceed button
-  const proceed = getByTestId("proceed");
-  expect(proceed).toBeDisabled();
-
-  // select enough items
-  for (let i = 0; i < MIN_ITEMS; i++) {
-    await fireEvent.click(getByTestId(values[i]));
-  }
-  expect(proceed).toBeEnabled();
-  await fireEvent.click(proceed);
-  expect(setPage.mock.calls.length).toBe(1);
-  expect(setPage.mock.calls[0][1]).toEqual(values.slice(0, MIN_ITEMS));
-
-  // select too many items
-  for (let i = MIN_ITEMS; i <= MAX_ITEMS; i++) {
-    await fireEvent.click(getByTestId(values[i]));
-  }
-  expect(proceed).toBeDisabled();
-
-  // unselect an item
-  await fireEvent.click(getByTestId(values[0]));
-  expect(proceed).toBeEnabled();
-  await fireEvent.click(proceed);
-  expect(setPage.mock.calls.length).toBe(2);
-  expect(setPage.mock.calls[1][1]).toEqual(values.slice(1, MAX_ITEMS + 1));
 });
